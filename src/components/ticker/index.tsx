547a764c76152619c6f1e0e67779f8e9a3ee12ac
mahmoud16h <Marymahmoud10>
2023-05-17 23:02:43 +0100

    add locale string to numbers

--- src/components/ticker/index.tsx
+++ src/components/ticker/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { useSelector} from "react-redux";
 import {getPricesSelector} from "../../store/prices/prices.selectors";
 
+const formatPrice = (price?: number) => {
+    if (!price) return null
+    return price.toLocaleString("en-US")
+}
+
 function TickerComponent() {
     const prices = useSelector(getPricesSelector)
 
     return (
         <div>
-            <div>BTC/USD: {prices?.BTCEUR?.LAST_PRICE}</div>
-            <div>BTC/EUR: {prices?.BTCEUR?.LAST_PRICE}</div>
-            <div>BTC/JPY: {prices?.BTCJPY?.LAST_PRICE}</div>
-            <div>BTC/GBP: {prices?.BTCGBP?.LAST_PRICE}</div>
+            <div>BTC/USD: {formatPrice(prices?.BTCEUR?.LAST_PRICE)}</div>
+            <div>BTC/EUR: {formatPrice(prices?.BTCEUR?.LAST_PRICE)}</div>
+            <div>BTC/JPY: {formatPrice(prices?.BTCJPY?.LAST_PRICE)}</div>
+            <div>BTC/GBP: {formatPrice(prices?.BTCGBP?.LAST_PRICE)}</div>
         </div>
     );
 }
